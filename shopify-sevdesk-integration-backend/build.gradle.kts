9bd4157b1c93075d2bd4a1f5f83c514b5e1c98b5
ArtHoffmann <Artur.Hoffmann@protonmail.com>
2024-07-04 23:25:05 +0200

    Initial commit with two modules

---
+++ shopify-sevdesk-integration-backend/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.1"
+	id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "shopify.sevdesk"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion.set(JavaLanguageVersion.of(22))
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.liquibase:liquibase-core")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+	testImplementation("org.springframework.security:spring-security-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ shopify-sevdesk-integration-backend/src/main/java/shopify/sevdesk/shopify_sevdesk_integration_backend/ShopifySevdeskIntegrationBackendApplication.java
@@ -0,0 +1,13 @@
+package shopify.sevdesk.shopify_sevdesk_integration_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopifySevdeskIntegrationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopifySevdeskIntegrationBackendApplication.class, args);
+	}
+
+}
