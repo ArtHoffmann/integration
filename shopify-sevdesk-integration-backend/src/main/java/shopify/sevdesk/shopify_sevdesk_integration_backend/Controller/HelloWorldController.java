ea3881d9ba9fbfd7fe14a0573abaa934684f790c
ArtHoffmann <Artur.Hoffmann@protonmail.com>
2024-07-23 17:41:28 +0200

    feature(): spring boot keycloak implementiert

--- shopify-sevdesk-integration-backend/src/main/java/shopify/sevdesk/shopify_sevdesk_integration_backend/Controller/HelloWorldController.java
+++ shopify-sevdesk-integration-backend/src/main/java/shopify/sevdesk/shopify_sevdesk_integration_backend/Controller/HelloWorldController.java
@@ -1,18 +1,32 @@
 package shopify.sevdesk.shopify_sevdesk_integration_backend.Controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
+
 @RestController
 public class HelloWorldController {
+
+    private static Logger logger = LogManager.getLogger(HelloWorldController.class);
+
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
+        logger.info("Hello World called");
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .body("{\"message\": \"Hello, World - test\"}");
     }
+
+    @GetMapping("/customers")
+    public String customers(Principal principal) {
+        return "Welcome, " + principal.getName() + "!";
+    }
+
 }
---
+++ shopify-sevdesk-integration-backend/src/main/java/shopify/sevdesk/shopify_sevdesk_integration_backend/authorization/AppConfig.java
@@ -0,0 +1,13 @@
+package shopify.sevdesk.shopify_sevdesk_integration_backend.authorization;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
